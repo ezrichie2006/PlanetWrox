bb6f1c3aecb0d40507959d30044ec4a0f66ec98a
ezrichie2006 <ezrichie2006@yahoo.com>
2015-08-20 08:01:24 -0700

    Initial Commit

---
+++ App_Code/Calculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Calculator
+/// </summary>
+public static class Calculator
+{
+    public static double Add(double a, double b)
+    {
+        return a + b;
+    }
+
+    public static double Subtract(double a, double b)
+    {
+        return a - b;
+    }
+
+    public static double Multiply(double a, double b)
+    {
+        return a * b;
+    }
+
+    public static double Divide(double a, double b)
+    {
+        return a / b;
+    }
+}
